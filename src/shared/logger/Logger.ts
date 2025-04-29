2b04a0b9fc0bd0fc1843c16b37aed355e1496e47
Wellington Reis <wellington.reis@blip.ai>
2025-04-28 23:56:18 -0300

    Feature: Use Loki log

--- src/shared/logger/Logger.ts
+++ src/shared/logger/Logger.ts
@@ -1,27 +1,18 @@
-export class Logger {
-  private static format(
-    level: string,
-    message: string,
-    meta?: Record<string, any>
-  ) {
-    const log = {
-      timestamp: new Date().toISOString(),
-      level,
-      message,
-      ...(meta || {}),
-    };
-    return JSON.stringify(log);
-  }
+import { createLogger, format, transports } from "winston";
+import LokiTransport from "winston-loki";
 
-  static info(message: string, meta?: Record<string, any>) {
-    console.log(this.format("info", message, meta));
-  }
-
-  static warn(message: string, meta?: Record<string, any>) {
-    console.warn(this.format("warn", message, meta));
-  }
-
-  static error(message: string, meta?: Record<string, any>) {
-    console.error(this.format("error", message, meta));
-  }
-}
+export const Logger = createLogger({
+  level: process.env.LOG_LEVEL || "info",
+  format: format.combine(format.timestamp(), format.json()),
+  transports: [
+    new LokiTransport({
+      host: process.env.LOKI_HOST || "http://localhost:3100",
+      labels: { job: process.env.LOKI_JOB_NAME || "email-service" },
+      json: true,
+      interval: 5,
+      timeout: 10000,
+      batching: true,
+    }),
+    new transports.Console(),
+  ],
+});
